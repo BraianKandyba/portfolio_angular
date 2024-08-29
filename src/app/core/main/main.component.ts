3d6e9d8baddd74364265fc49901d6e325787f597
braian kandyba <braian_1721@hotmail.com>
2024-08-28 21:29:28 -0300

    segundo commit

--- src/app/core/main/main.component.ts
+++ src/app/core/main/main.component.ts
@@ -1,20 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.scss'
 })
-export class MainComponent {
-    menuOpen = false;
+export class MainComponent implements OnInit {
 
+    ngOnInit(): void {
+        this.handleScroll(); 
+    }
+
+    menuOpen = false;
     toggleMenu() {
         this.menuOpen = !this.menuOpen;
     }
-
     closeMenu() {
         this.menuOpen = false;
     }
 
+    
+    
+    @HostListener('window:scroll', [])
+    onWindowScroll(): void {
+        this.handleScroll();
+    }
+    
+    
+    private isVisible(element: HTMLElement): boolean {
+        const rect = element.getBoundingClientRect();
+        return rect.top < window.innerHeight && rect.bottom >= 0;
+    }
+    
+    private handleScroll(): void {
+        const contents = document.querySelectorAll<HTMLElement>('.content');
+        contents.forEach(content => {
+            if (this.isVisible(content)) {
+            content.classList.add('visible');
+            } else {
+            content.classList.remove('visible');
+        }
+        });
+    }
 
-}
+
+} 
